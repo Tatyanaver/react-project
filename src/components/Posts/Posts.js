ca1989daed959ee7e66a1f7c021129d8a0f10d92
Tatyanaver <91628615+Tatyanaver@users.noreply.github.com>
2022-01-24 15:27:45 +0200

    Update Posts.js

---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React, {useEffect, useState} from 'react';
+
+import Post from "../Post/Post";
+
+
+const Posts = ({posts}) => {
+    return (
+        <div>
+            {posts.map(post => <Post key={post.id} post={post}/>)}
+        </div>
+    );
+};
+
+export default Posts;
